d82ce392f3953c8c1680572e1e94b9f53177ca36
EliasDeProgramacion <asuntagallegos160@gmail.com>
2026-07-19 16:20:41 -0500

    Create DatosNivel4.java

---
+++ src/Events/EarthQuake/DatosNivel4.java
@@ -0,0 +1,13 @@
+package sismos.nivel_04;
+ import sismos.SismoBase;
+ public class DatosNivel4 {
+     public static SismoBase obtener() {
+         return new SismoBase(
+             4,
+             80,
+             15.0,
+             "",
+             "Días o semanas"
+         );
+     }
+ }
