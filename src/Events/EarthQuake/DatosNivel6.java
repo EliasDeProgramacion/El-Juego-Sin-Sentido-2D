c47b6ed5b3728f62576623dc947232f897fe9f9b
EliasDeProgramacion <asuntagallegos160@gmail.com>
2026-07-19 16:23:20 -0500

    Create DatosNivel6.java

---
+++ src/Events/EarthQuake/DatosNivel6.java
@@ -0,0 +1,13 @@
+package sismos.nivel_06;
+ import sismos.SismoBase;
+ public class DatosNivel6 {
+     public static SismoBase obtener() {
+         return new SismoBase(
+             6,
+             75,
+             24.0,
+             "",
+             "6 a 7 meses"
+         );
+     }
+ }
