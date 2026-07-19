14ff072e5745d40d416bbff146ec1f3b7e51fe9d
EliasDeProgramacion <asuntagallegos160@gmail.com>
2026-07-19 16:21:46 -0500

    Create DatosNivel5.java

---
+++ src/Events/EarthQuake/DatosNivel5.java
@@ -0,0 +1,13 @@
+package sismos.nivel_05;
+ import sismos.SismoBase;
+ public class DatosNivel5 {
+     public static SismoBase obtener() {
+         return new SismoBase(
+             5,
+             80,
+             17.0,
+             "",
+             "Un mes"
+         );
+     }
+ }
