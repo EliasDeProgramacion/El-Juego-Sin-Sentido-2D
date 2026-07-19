5dac6c6e3eee3a086c93f9232de09aadee6dc8c5
EliasDeProgramacion <asuntagallegos160@gmail.com>
2026-07-19 16:24:42 -0500

    Create DatosNivel7.java

---
+++ src/Events/EarthQuake/DatosNivel7.java
@@ -0,0 +1,13 @@
+package sismos.nivel_07;
+ import sismos.SismoBase;
+ public class DatosNivel7 {
+     public static SismoBase obtener() {
+         return new SismoBase(
+             7,
+             70,
+             40.0,
+             "",
+             "2 a 10 años"
+         );
+     }
+ }
