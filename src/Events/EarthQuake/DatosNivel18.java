667fe3bf1c6d936b7e9439528b5b25e13945ed52
EliasDeProgramacion <asuntagallegos160@gmail.com>
2026-07-19 16:44:11 -0500

    Create DatosNivel18.java

---
+++ src/Events/EarthQuake/DatosNivel18.java
@@ -0,0 +1,13 @@
+package sismos.nivel_18;
+ import sismos.SismoBase;
+ public class DatosNivel18 {
+     public static SismoBase obtener() {
+         return new SismoBase(
+             18,
+             15,
+             200.0,
+             "Cataclismo Extremo",
+             "Cincuentos de Millones de Años"
+         );
+     }
+ }
