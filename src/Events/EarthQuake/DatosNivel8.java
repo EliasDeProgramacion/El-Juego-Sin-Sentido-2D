d5e007b639b2b8e33d470a3589bddbdc68f4188a
EliasDeProgramacion <asuntagallegos160@gmail.com>
2026-07-19 16:25:53 -0500

    Create DatosNivel8.java

---
+++ src/Events/EarthQuake/DatosNivel8.java
@@ -0,0 +1,13 @@
+package sismos.nivel_08;
+ import sismos.SismoBase;
+ public class DatosNivel8 {
+     public static SismoBase obtener() {
+         return new SismoBase(
+             8,
+             65,
+             60.0,
+             "",
+             "10 a 60 años"
+         );
+     }
+ }
