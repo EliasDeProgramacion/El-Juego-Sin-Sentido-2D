ef37d891cd470da45c9f6b723e6bb5770cdcac03
EliasDeProgramacion <asuntagallegos160@gmail.com>
2026-07-19 16:37:29 -0500

    Update and rename Datos nivel.java to DatosNivel14.java

---
+++ src/Events/EarthQuake/DatosNivel14.java
@@ -0,0 +1,13 @@
+package sismos.nivel_14;
+ import sismos.SismoBase;
+ public class DatosNivel13 {
+     public static SismoBase obtener() {
+         return new SismoBase(
+             14,
+             35,
+             160.0,
+             "Cataclismo Mayor",
+             "Cincuentos de miles de años"
+         );
+     }
+ }
