ab0852ac1230083f45a1fd796e607821c3faeaa8
EliasDeProgramacion <asuntagallegos160@gmail.com>
2026-07-19 16:16:54 -0500

    Create DatosNivel2.java

---
+++ src/Events/EarthQuake/DatosNivel2.java
@@ -0,0 +1,13 @@
+package sismos.nivel_02;
+ import sismos.SismoBase;
+ public class DatosNivel2 {
+     public static SismoBase obtener() {
+         return new SismoBase(
+             2,
+             95,
+             10.0,
+             "",
+             "3 a 4 horas"
+         );
+     }
+ }
