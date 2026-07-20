b29e3ea54dda9d06ab8b1dca86f2498e28c7741b
EliasDeProgramacion <asuntagallegos160@gmail.com>
2026-07-20 12:51:39 -0500

    Create Clima despejado.java

---
+++ src/Events/Wheather/Clima despejado.java
@@ -0,0 +1,15 @@
+package clima;
+
+public class ClimaDespejado extends ClimaBase {
+    public ClimaDespejado() {
+        super(
+            "Despejado",
+            0,     
+            0,      
+            100,       
+            false,
+            false,
+            "Estado base: cielo limpio, sin efectos especiales"
+        );
+    }
+}
