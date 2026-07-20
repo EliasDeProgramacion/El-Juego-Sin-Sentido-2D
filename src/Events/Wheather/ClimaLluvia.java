f785154fba24b517b2375efd945c2ad885623992
EliasDeProgramacion <asuntagallegos160@gmail.com>
2026-07-20 12:59:12 -0500

    Create ClimaLluvia.java

---
+++ src/Events/Wheather/ClimaLluvia.java
@@ -0,0 +1,15 @@
+package clima;
+
+public class ClimaLluvia extends ClimaBase {
+    public ClimaLluvia() {
+        super(
+            "Lluvia",
+            120,     
+            1200,      
+            45,       
+            true,
+            true,
+            "Luvia, gotas caen del cielo sin parar o si puede parar"
+        );
+    }
+}
