39bd4baa423324ce36acaeb314d4fab09a625bcf
EliasDeProgramacion <asuntagallegos160@gmail.com>
2026-07-20 02:54:01 -0500

    Create BloqueMinaCarbon.java

---
+++ src/Bloques/BloqueMinaCarbon.java
@@ -0,0 +1,14 @@
+package bloques;
+
+public class BloqueMinaCarbon extends BloqueBase {
+    public BloqueTierra() {
+        super(
+            "Mina De Carbon",
+            true,
+            true,
+            "Pico",
+            "Carbon",
+            "MinaDeCarbon_01"
+        );
+    }
+}
