382e3926ed5222a315d7198531174f0f7510cbd3
EliasDeProgramacion <asuntagallegos160@gmail.com>
2026-07-20 03:28:21 -0500

    Create BloqueMinaHierro.java

---
+++ src/Bloques/BloqueMinaHierro.java
@@ -0,0 +1,18 @@
+package bloques;
+
+public class BloqueMinaDeHierro extends BloqueBase {
+    public BloqueMinaDeHierro() {
+        super(
+            "Mina de Hierro",
+             8,
+             8,
+            true,
+            true,
+            "Pico"
+            "Mena De Hierro",
+            "Roca",
+            50,
+            "MinaDeHierro_01"
+        );
+    }
+}
