5311a0fb6166bbd03bded2fc3fef96a85c31516c
EliasDeProgramacion <asuntagallegos160@gmail.com>
2026-07-20 02:25:27 -0500

    Create BloqueTierra.java

---
+++ src/Bloques/BloqueTierra.java
@@ -0,0 +1,13 @@
+package bloques;
+
+public class BloqueTierra extends BloqueBase {
+    public BloqueTierra() {
+        super(
+            "Tierra",
+            true,
+            true,
+            "Tierra",
+            "tierra_01"
+        );
+    }
+}
