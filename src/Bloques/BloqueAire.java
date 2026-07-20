a63ed01dc8035e98e0b7ea5dc3cbad3837ce5f86
EliasDeProgramacion <asuntagallegos160@gmail.com>
2026-07-20 02:33:42 -0500

    Create BloqueAire.java

---
+++ src/Bloques/BloqueAire.java
@@ -0,0 +1,15 @@
+package bloques;
+
+public class BloqueAire extends BloqueBase {
+    public BloqueAire() {
+        super(
+            "Aire",
+            8,
+            8,
+            false,
+            false,
+            "Aire",
+            "aire_01"
+        );
+    }
+}
