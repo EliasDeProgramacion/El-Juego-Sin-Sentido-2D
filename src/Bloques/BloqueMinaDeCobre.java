42a8f550a7d0dd8b61de3defef1d9e405c9a483a
EliasDeProgramacion <asuntagallegos160@gmail.com>
2026-07-20 03:31:53 -0500

    Create BloqueMinaDeCobre.java

---
+++ src/Bloques/BloqueMinaDeCobre.java
@@ -0,0 +1,17 @@
+package bloques;
+
+public class BloqueMinaDeCobre extends BloqueBase {
+    public BloqueMinaDeCobre() {
+        super(
+            "Mina De Cobre",
+             8,
+             8,
+            true,
+            true,
+            "MenaDeCobre",
+            "Roca",
+            40,
+            "MenaDeCobre_01"
+        );
+    }
+}
