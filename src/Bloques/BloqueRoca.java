d53b471e87d948265b6bf1ccf9eba6d52c25fd78
EliasDeProgramacion <asuntagallegos160@gmail.com>
2026-07-20 02:26:27 -0500

    Create BloqueRoca.java

---
+++ src/Bloques/BloqueRoca.java
@@ -0,0 +1,13 @@
+package bloques;
+
+public class BloqueRoca extends BloqueBase {
+    public BloqueRoca() {
+        super(
+            "Roca",
+            true,
+            true,
+            "Roca",
+            "roca_01"
+        );
+    }
+}
