1e2f298be3865008abc58ba5d30a22c58a960105
EliasDeProgramacion <asuntagallegos160@gmail.com>
2026-07-20 03:34:25 -0500

    Create MinaDeDiamante.java

---
+++ src/Bloques/MinaDeDiamante.java
@@ -0,0 +1,17 @@
+package bloques;
+
+public class BloqueMinaDeDiamante extends BloqueBase {
+    public BloqueMinaDeDiamante() {
+        super(
+            "MinaDeDiamante",
+             8,
+             8,
+            true,
+            true,
+            "Diamante",
+            "Roca",
+            30,
+            "MinaDeDiamante_01"
+        );
+    }
+}
