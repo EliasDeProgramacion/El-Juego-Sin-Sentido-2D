704f88cf6ab5bb04b0e1ea1ae34c7dbb0765c894
EliasDeProgramacion <asuntagallegos160@gmail.com>
2026-07-20 02:49:40 -0500

    Create BloquePino.java

---
+++ src/Bloques/BloquePino.java
@@ -0,0 +1,14 @@
+package bloques;
+
+public class BloquePino extends BloqueBase {
+    public BloquePino() {
+        super(
+            "Pino",
+            true,
+            true,
+            "Hacha",
+            "Madera",
+            "Pino_01"
+        );
+    }
+}
