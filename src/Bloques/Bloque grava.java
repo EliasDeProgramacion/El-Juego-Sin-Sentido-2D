d62945f65c4abb77fd420ac11fa9457bc8c6d678
EliasDeProgramacion <asuntagallegos160@gmail.com>
2026-07-20 02:58:37 -0500

    Create Bloque grava.java

---
+++ src/Bloques/Bloque grava.java
@@ -0,0 +1,15 @@
+package bloques;
+
+public class BloqueTierra extends BloqueBase {
+    public BloqueTierra() {
+        super(
+            "Tierra",
+            true,
+            true,
+            "Grava",
+            "Pedernal",
+            50,
+            "Grava_01"
+        );
+    }
+}
