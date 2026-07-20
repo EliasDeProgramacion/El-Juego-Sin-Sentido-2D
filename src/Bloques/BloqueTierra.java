d9effc1b555c8eddab5474c9bd88b37516bfbde0
EliasDeProgramacion <asuntagallegos160@gmail.com>
2026-07-20 02:30:52 -0500

    Update BloqueTierra.java

--- src/Bloques/BloqueTierra.java
+++ src/Bloques/BloqueTierra.java
@@ -4,6 +4,8 @@ public class BloqueTierra extends BloqueBase {
     public BloqueTierra() {
         super(
             "Tierra",
+             8,
+             8,
             true,
             true,
             "Tierra",
