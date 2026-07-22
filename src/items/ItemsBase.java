e8b9d368af2e8d9dfc62021eda361c562d0d2043
EliasDeProgramacion <asuntagallegos160@gmail.com>
2026-07-22 00:46:06 -0500

    Update ItemsBase.java

--- src/items/ItemsBase.java
+++ src/items/ItemsBase.java
@@ -1,12 +1,14 @@
-package items 
+package items;
    public class ItemBase {
      public String id;
      public String nombre;
      public String descripcion;
      public int nivel;
 
-     public class (String id, String nombre) {
+     public ItemBase (String id, String nombre) {
        this.id = id;
        this.nombre = nombre;
        this.descripcion = descripcion;
        this.nivel = nivel;
+     }
+ }
