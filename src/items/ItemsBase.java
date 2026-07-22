28cd05b3f7c65680ce3f8eab3b3ed19661df3300
EliasDeProgramacion <asuntagallegos160@gmail.com>
2026-07-22 00:42:59 -0500

    Update and rename ItemBase.java to ItemsBase.java

---
+++ src/items/ItemsBase.java
@@ -0,0 +1,12 @@
+package items 
+   public class ItemBase {
+     public String id;
+     public String nombre;
+     public String descripcion;
+     public int nivel;
+
+     public class (String id, String nombre) {
+       this.id = id;
+       this.nombre = nombre;
+       this.descripcion = descripcion;
+       this.nivel = nivel;
